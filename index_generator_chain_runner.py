8626a590163d5f79063119de1c72101414c4c244
Andrei Damintcev <Andrei.Damintcev@sbb.ch>
2021-10-20 15:53:01 +0300

    small refactoring

--- index_generator_chain_runner.py
+++ index_generator_chain_runner.py
@@ -1,53 +1,39 @@
-import getopt
+import argparse
 import os
 import subprocess
 import sys
 import zipfile
 from datetime import datetime
+from os import path
 
 import requests
 
-BASE_PATH = os.path.normpath(os.path.dirname(__file__))
-DATA_DIR_PATH = os.path.normpath(os.path.join(os.path.dirname(__file__), 'data'))
-PROJECT_DIR = ''
-PROJECT_ID = ''
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='index_generator_chain_runner.py --project-dir <project-dir> '
+                                                 '--project-id <project-id>')
+    parser.add_argument('-pd', '--project-dir', help='Project directory for indexing ', required=True)
+    parser.add_argument('-pi', '--project-id', help='Project Id', required=True)
+    # parser.add_argument('-pi', '--project-id', help='Project Id', required=True)
+    args = parser.parse_args()
 
-def parse_arguments(argv):
-    project_dir = ''
-    project_id = ''
-    try:
-        opts, args = getopt.getopt(argv, "h:p:i:", ["project-dir=", "project-id="])
-    except getopt.GetoptError:
-        print('index_generator_chain_runner.py --project-dir <project-dir> --project-id <project-id>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('index_generator_chain_runner.py --project-dir <project-dir> --project-id <project-id>')
-            sys.exit()
-        elif opt in ("-p", "--project-dir"):
-            project_dir = arg
-        elif opt in ("-i", "--project-id"):
-            project_id = arg
-    return project_dir, project_id
+    return args.project_dir, args.project_id
 
 
 def get_commit_id():
-    process = subprocess.Popen(["git", "show", "-s", "--format=%H"], cwd=PROJECT_DIR, stdout=subprocess.PIPE)
+    process = subprocess.Popen(["git", "show", "-s", "--format=%H"], cwd=project_dir, stdout=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return str(stdout).replace('b\'', '').replace('\\n\'', '')
 
 
 def execute_docker_operation():
     process = subprocess.Popen(["docker", "run",
-                                "-v", PROJECT_DIR + ":/var/project",
+                                "-v", project_dir + ":/var/project",
                                 "-v", DATA_DIR_PATH + ":/shared-index/output",
                                 "-e", "COMMIT_ID=" + commit_id,
-                                "-e", "PROJECT_ID=" + PROJECT_ID,
+                                "-e", "PROJECT_ID=" + project_id,
                                 "damintsew/indexer-2021.2.3"],
-                               cwd=".",
-                               stdout=subprocess.PIPE,
-                               # env=d,
+                               cwd=".", stdout=subprocess.PIPE,
                                universal_newlines=True)
     while True:
         output = process.stdout.readline()
@@ -70,20 +56,22 @@ def zipdir(path, ziph):
                                        os.path.join(path, '.')))
 
 
-def remove_files(path):
-    for root, dirs, files in os.walk(path):
+def remove_files(base_path):
+    for root, dirs, files in os.walk(base_path):
         for file in files:
-            os.remove(os.path.relpath(os.path.join(path, file)))
+            os.remove(os.path.relpath(os.path.join(base_path, file)))
 
 
-if __name__ == "__main__":
-    PROJECT_DIR, PROJECT_ID = parse_arguments(sys.argv[1:])
+BASE_PATH = path.normpath(path.dirname(__file__))
+DATA_DIR_PATH = path.normpath(path.join(path.dirname(__file__), 'data'))
+DATA_TO_SEND_DIR_PATH = path.normpath(path.join(path.dirname(__file__), 'data_to_send'))
 
+project_dir, project_id = parse_arguments()
 commit_id = get_commit_id()
 
-print('PROJECT_DIR = ' + PROJECT_DIR)
-print('PROJECT_ID = ' + PROJECT_ID)
-print('CommitId = ' + commit_id)
+print('project_dir = ' + project_dir)
+print('project_id = ' + project_id)
+print('commit_id = ' + commit_id)
 
 print('Started Indexing ' + datetime.now().strftime("%H:%M:%S"))
 execute_docker_operation()
@@ -93,16 +81,13 @@ zipf = zipfile.ZipFile('data_to_send\\index.zip', 'w', zipfile.ZIP_DEFLATED)
 zipdir(DATA_DIR_PATH, zipf)
 zipf.close()
 
-with open("data_to_send\\index.zip", "rb") as a_file:
+with open(DATA_TO_SEND_DIR_PATH, "rb") as a_file:
     file_dict = {"file": a_file}
-    # response = requests.post("http://localhost:8125/manage/upload",
-    #                          params={'commitId': commit_id, 'projectId': PROJECT_ID},
-    #                          files=file_dict)
     response = requests.post("http://10.216.1.51:8125/manage/upload",
-                             params={'commitId': commit_id, 'projectId': PROJECT_ID},
+                             params={'commitId': commit_id, 'projectId': project_id},
                              timeout=600,
                              files=file_dict)
     print(response)
 
 remove_files(DATA_DIR_PATH)
-remove_files(BASE_PATH + '/data_to_send')
+remove_files(DATA_TO_SEND_DIR_PATH)
