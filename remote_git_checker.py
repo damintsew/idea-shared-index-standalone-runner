e15988f366c89c70862f670c7bf8c6170cb8aa6d
e523714 <andrei.damintcev@sbb.ch>
2021-11-28 23:39:03 +0300

    small updates

--- remote_git_checker.py
+++ remote_git_checker.py
@@ -6,7 +6,7 @@ import chain_runner
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='index_generator_chain_runner.py --project-dir <project-dir> '
-                                                 '--project-id <project-id>')
+                                                 '--project-id <project-id> --branch <remote branch to watch>')
     parser.add_argument('-pd', '--project-dir', help='Project directory for indexing ', required=True)
     parser.add_argument('-pi', '--project-id', help='Project Id', required=True)
     parser.add_argument('-b', '--branch', help='Branch', required=False)
